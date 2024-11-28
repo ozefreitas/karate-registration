462fa766bd6035e28795cac617a037efc976b7d6
Jose Freitas <pg42872@alunos.uminho.pt>
2024-11-28 00:06:51 +0000

    [ADD] options button to show or hide registration controls

--- registration/static/registration/js/main.js
+++ registration/static/registration/js/main.js
@@ -6,3 +6,12 @@ const toggleAthleteDetails = (athleteID) => {
         athleteDiv.style.display = "none";
     }
 }
+
+const toggleRegistrationOptions = (athleteID) => {
+    const athleteDiv = document.getElementById(`buttons-container-${athleteID}`)
+    if (athleteDiv.style.display === "none" || athleteDiv.style.display === "") {
+        athleteDiv.style.display = "flex";
+    } else {
+        athleteDiv.style.display = "none";
+    }
+}
