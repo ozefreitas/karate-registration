d23999ab9fe38310d5868fe71b31725c9a6db213
ozefreitas <jpsfreitas12@gmail.com>
2025-01-04 17:57:49 +0000

    [UPDATE] settings and base with link to recieve the icon

--- karate_registration/settings.py
+++ karate_registration/settings.py
@@ -128,6 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "registration/static"]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
