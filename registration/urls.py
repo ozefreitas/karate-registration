340e552f2b577119f18e8a03d65ecf02d7221f77
ozefreitas <jpsfreitas12@gmail.com>
2025-02-13 14:57:04 +0000

    [UPDATE] change submit only buttons to more options and remove "thanks" page

--- registration/urls.py
+++ registration/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     path('teams/', views.teams, name="registration-teams"),
     path('form/', views.form, name="registration-form"),
     path('teams_form/', views.team_form, name="registration-teams-form"),
-    path('thanks/', views.thanks, name="registration-thanks"),
     path('wrong/', views.wrong, name="registration-wrong"),
     path('delete/<str:type>/<int:id>/', views.delete, name="registration-delete"),
     path('update_registration/<str:type>/<int:id>/', views.update, name="registration-update"),
