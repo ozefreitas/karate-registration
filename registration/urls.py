746e64963553ea1ee5be1c33da97b76c438ebbba
Jose Freitas <pg42872@alunos.uminho.pt>
2025-02-17 22:21:50 +0000

    [UPDATE] major update to the teams integration into the platform

--- registration/urls.py
+++ registration/urls.py
@@ -7,8 +7,7 @@ urlpatterns = [
     path('athletes/', views.athletes, name="registration-athletes"),
     path('teams/', views.teams, name="registration-teams"),
     path('form/', views.form, name="registration-form"),
-    path('teams_form/', views.team_form, name="registration-teams-form"),
-    path('wrong/', views.wrong, name="registration-wrong"),
+    path('teams_form/<str:match_type>', views.team_form, name="registration-teams-form"),
     path('delete/<str:type>/<int:id>/', views.delete, name="registration-delete"),
     path('update_registration/<str:type>/<int:id>/', views.update, name="registration-update"),
 ]
--- registration/utils/utils.py
+++ registration/utils/utils.py
@@ -1,12 +1,30 @@
 from datetime import datetime
 from django.contrib import messages
+import requests
 
-def range_decoder(some_range: str):
+def range_decoder(some_range: list):
+    """Function that returns a range 
+
+    Args:
+        some_range (list): _description_
+
+    Returns:
+        range object: The range from the given range
+    """
     some_range = some_range.split("-")
     some_range = range(int(some_range[0]), int(some_range[1]) + 1)
     return some_range
 
-def check_match_type(response):
+
+def check_match_type(response: requests) -> tuple:
+    """Function that checks the checboxes for the match type selection
+
+    Args:
+        response (_type_): _description_
+
+    Returns:
+        tuple: A list containing the selected match types
+    """
     matches_list= []
     error = ""
     if response.POST.dict().get("match_type_kata", 0) == 0 and response.POST.dict().get("match_type_kumite", 0) == 0:
@@ -18,7 +36,16 @@ def check_match_type(response):
             matches_list.append("kumite")
     return matches_list, error
 
-def get_comp_age(date_of_birth):
+
+def get_comp_age(date_of_birth: datetime) -> int:
+    """Function that return the age of an athlete by the begining of the year
+
+    Args:
+        date_of_birth (datetime): The birth date of the athlete. Must be an instance of datetime 
+
+    Returns:
+        int: The age as an int
+    """
     year_of_birth = date_of_birth.year
     date_now = datetime.now()
     age_at_comp = (date_now.year) - year_of_birth
@@ -28,7 +55,20 @@ def get_comp_age(date_of_birth):
     #     age_at_comp -= 1
     # return age_at_comp
 
-def check_athlete_data(data, age_at_comp, grad_rules, category_rules, extra_data = None):
+
+def check_athlete_data(data, age_at_comp: int, grad_rules: dict, category_rules: dict, extra_data = None) -> list:
+    """Receives the form from the view, processes the data to check any error or rules infrigements 
+
+    Args:
+        data (form): The form data
+        age_at_comp (int): _description_
+        grad_rules (dict): _description_
+        category_rules (dict): _description_
+        extra_data (_type_, optional): _description_. Defaults to None.
+
+    Returns:
+        list: _description_
+    """
     errors = []
     for age_range, grad in grad_rules.items():
         age_range = range_decoder(age_range)
@@ -51,6 +91,44 @@ def check_athlete_data(data, age_at_comp, grad_rules, category_rules, extra_data
             errors.append("Os escalões femininos não são dividos por pesos")
 
     return errors
+
+
+def check_teams_data(data) -> list:
+    """Receives the form from the view, processes the data to check any error or rules infrigements 
+
+    Args:
+        data (form): The form data
+
+    Returns:
+        list: _description_
+    """
+    CATEGORY_RULES = {
+        "Veterano +35": "Sénior",
+        "Veterano +50": "Sénior",
+        "Sénior": "Júnior",
+        "Júnior": "Cadete",
+        "Cadete": "Juvenil",
+        "Juvenil": "Iniciado",
+        "Iniciado": "Infantil",
+        "Infantil": "Infantil",
+    }
+
+    athletes_set = [value for key, value in data.cleaned_data.items() if key.startswith("athlete") and value is not None]
+    errors = []
+    category_up = 0
+    team_cat = data.cleaned_data.get("category", 0)
+    for athlete in athletes_set:
+        if athlete.category != team_cat and CATEGORY_RULES[team_cat] != athlete.category:
+            errors.append(f"{athlete.first_name} {athlete.last_name} não pode participar nesta prova")
+
+        if athlete.category != team_cat and CATEGORY_RULES[team_cat] == athlete.category:
+            category_up += 1
+        
+        if category_up > 1:
+            errors.append("Não pode ter mais do que um atleta a subir de escalão")
+    print(errors)
+    return errors
+
 
 def check_filter_data(request, filter_form, dojo_object):
     not_found = False
