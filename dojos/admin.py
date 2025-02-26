a489d00054106552148980f234105bf8edda8871
ozefreitas <jpsfreitas12@gmail.com>
2025-02-26 15:21:29 +0000

    [ADD] search fields for admin page

--- dojos/admin.py
+++ dojos/admin.py
@@ -4,8 +4,9 @@ from .models import CompetitionDetail, FeedbackData, Profile
 # Register your models here.
 
 class CompetitionDetailAdmin(admin.ModelAdmin):
-  list_display = ("name", "location", "season")
-
+    list_display = ("name", "location", "season")
+    search_fields = ("name", "location")
+    ordering = ("start_registration", "competition_date")
 
 admin.site.register(CompetitionDetail, CompetitionDetailAdmin)
 admin.site.register(FeedbackData)
--- registration/admin.py
+++ registration/admin.py
@@ -3,9 +3,19 @@ from .models import Athlete, Dojo, Team, ArchivedAthlete, Individual, Coach
 
 # Register your models here.
 
-admin.site.register(Athlete)
+class AthleteAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "category", "match_type", "gender")
+    search_fields = ("first_name", "last_name", "category", "match_type", "dojo")
+
+
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ("category", "match_type", "gender", "team_number")
+    search_fields = ("category", "match_type", "gender", "dojo")
+
+
+admin.site.register(Athlete, AthleteAdmin)
 admin.site.register(Dojo)
-admin.site.register(Team)
+admin.site.register(Team, TeamAdmin)
 admin.site.register(ArchivedAthlete)
 admin.site.register(Coach)
 admin.site.register(Individual)
