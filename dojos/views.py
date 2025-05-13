24e75a6b36b277810beef9f3e72c13e386d94c8c
ozefreitas <jpsfreitas12@gmail.com>
2025-05-13 21:49:57 +0100

    [ADD] isAdminUser

--- dojos/views.py
+++ dojos/views.py
@@ -22,7 +22,7 @@ from dojos import serializers
 from rest_framework import viewsets, filters, status
 from rest_framework.decorators import authentication_classes, permission_classes
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework. views import APIView
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, action
--- registration/views.py
+++ registration/views.py
@@ -20,7 +20,7 @@ from rest_framework.decorators import api_view, action
 from rest_framework.response import Response
 from rest_framework.decorators import authentication_classes, permission_classes
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 import registration.serializers as serializers
 
 # views for the athlets registrations
