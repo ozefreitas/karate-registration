fa04c82d3857dd21d751aa529dca6305cc129cb3
ozefreitas <jpsfreitas12@gmail.com>
2025-05-31 14:38:48 +0100

    [ADD] new filter for athletes from category and gender

--- registration/filters.py
+++ registration/filters.py
@@ -19,9 +19,19 @@ class AthletesFilters(filters.FilterSet):
     """Filter Atlhetes not in comp_id"""
     not_in_competition = filters.CharFilter(field_name='not_in_competition',
                                               method='filter_athletes_not_in_competition')
+    in_category = filters.CharFilter(field_name='in_category',
+                                              method='filter_athletes_in_category')
+    in_gender = filters.CharFilter(field_name='in_gender',
+                                              method='filter_athletes_in_gender')
 
     def filter_athletes_not_in_competition(self, queryset, name, value):
         return queryset.exclude(individual__competition_id=value)
+    
+    def filter_athletes_in_category(self, queryset, name, value):
+        return queryset.filter(category=value)
+    
+    def filter_athletes_in_gender(self, queryset, name, value):
+        return queryset.filter(gender=value)
 
     class Meta:
         model = Athlete
