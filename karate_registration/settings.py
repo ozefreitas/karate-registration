a5e7e4ad4adb96b0957d8b47c92e385bb8867978
ozefreitas <jpsfreitas12@gmail.com>
2025-06-01 22:31:00 +0100

    [ADD] pagination

--- karate_registration/settings.py
+++ karate_registration/settings.py
@@ -84,8 +84,8 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ],
-    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    # 'PAGE_SIZE': 100
+    'DEFAULT_PAGINATION_CLASS': 'dojos.pagination.StandardResultsSetPagination',
+    'PAGE_SIZE': 10
 }
 
 TEMPLATES = [
