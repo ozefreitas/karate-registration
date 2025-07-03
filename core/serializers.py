e8efc634c358a6cbdc62f678148d8ffb109c6596
ozefreitas <jpsfreitas12@gmail.com>
2025-07-03 23:43:35 +0100

    [ADD] serializer for profile data

--- core/serializers.py
+++ core/serializers.py
@@ -5,3 +5,9 @@ class UsersSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.User
         fields = ["id", "username", "role"]
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.User
+        exclude = ["password", ]
