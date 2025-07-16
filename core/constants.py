1ea06b6736df2302bcaaa6315474a9c000d64c48
ozefreitas <jpsfreitas12@gmail.com>
2025-07-16 23:24:00 +0100

    [REFACTOR] contants to seperate file in core app

---
+++ core/constants.py
@@ -0,0 +1,45 @@
+CATEGORIES = {
+    "Infantil": "Infantil",
+    "Iniciado": "Iniciado",
+    "Juvenil": "Juvenil",
+    "Cadete": "Cadete",
+    "Júnior": "Júnior",
+    "Sénior": "Sénior",
+    "Veterano +35": "Veterano +35",
+    "Veterano +50": "Veterano +50"
+}
+
+MATCHES = {
+        "kata": "Kata",
+        "kumite": "Kumite"
+}
+
+GRADUATIONS = {
+        "15": "9º Kyu",
+        "14.5": "8º Kyu Kari",
+        "14": "8º Kyu",
+        "13.5": "7º Kyu Kari",
+        "13": "7º Kyu",
+        "12.5": "6º Kyu Kari",
+        "12": "6º Kyu",
+        "11.5": "5º Kyu Kari",
+        "11": "5º Kyu",
+        "10.5": "4º Kyu Kari",
+        "10": "4º Kyu",
+        "9.5": "3º Kyu Kari",
+        "9": "3º Kyu",
+        "8": "2º Kyu",
+        "7": "1º Kyu",
+        "6": "1º Dan",
+        "5": "2º Dan",
+        "4": "3º Dan",
+        "3": "4º Dan",
+        "2": "5º Dan",
+        "1": "6º Dan",
+    }
+
+GENDERS = {
+        "Masculino": "Masculino",
+        "Feminino": "Feminino",
+        "Misto": "Misto",
+    }
