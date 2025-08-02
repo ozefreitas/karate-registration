499813e76043ab0294bbdb21988831036195df22
ozefreitas <jpsfreitas12@gmail.com>
2025-08-02 16:55:29 +0100

    [UPDATE] mutations

--- core/urls.py
+++ core/urls.py
@@ -3,9 +3,18 @@ from . import views
 from dojos.views import rules
 from rest_framework import routers
 
+from rest_framework.authtoken.views import obtain_auth_token
+
 router = routers.DefaultRouter()
 router.register(r'categories', views.CategoriesViewSet, basename='categories')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('generate_token/', views.sign_up_token, name="generate-token"),
+    path('get_token_username/', views.get_token_username, name="token-username"),
+    path('register_user/', views.RegisterView.as_view(), name="register-with-token"),
+    path('current_season/', views.current_season, name="current-season"),
+    path('login/', obtain_auth_token, name="user-login"),
+    path('me/', views.UserDetailView.as_view(), name="user-detail"),
+    path('logout/', views.LogoutView.as_view(), name="user-logout"),
 ]
