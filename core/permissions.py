5d0c4b0ddbf7c2a79184ecaa2a23c6f39c333142
ozefreitas <jpsfreitas12@gmail.com>
2025-08-15 12:58:17 +0100

    [ADD] permissions for request acount

--- core/permissions.py
+++ core/permissions.py
@@ -14,11 +14,18 @@ class IsAuthenticatedOrReadOnly(BasePermission):
 
 class IsNationalForPostDelete(BasePermission):
     """
-    Allows POST and DELETE only for users with role 'national'.
+    Allows POST and DELETE only for users with role 'main_admin'.
     Other methods are allowed (or denied) based on default behavior.
     """
-
     def has_permission(self, request, view):
         if request.method in ['POST', 'DELETE']:
             return bool(request.user and request.user.is_authenticated and request.user.role == 'main_admin')
+        return True
+
+
+class IsUnauthenticatedForPost(BasePermission):
+    """Allows access to POST for anyone. DELETE and GET are retricted to admin roles"""
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS or request.method in ["DELETE"]:
+            return bool(request.user and request.user.is_authenticated and (request.user.role == 'main_admin' or request.user.role == 'superuser'))
         return True
