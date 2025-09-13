959c303a79961dd2322ad2ed2701271ea51920e3
ozefreitas <jpsfreitas12@gmail.com>
2025-09-13 12:25:04 +0100

    [UPDATE] events without categories filters athletes out that are in one discipline

--- registration/filters.py
+++ registration/filters.py
@@ -13,9 +13,20 @@ class AthletesFilters(filters.FilterSet):
     def filter_athletes_not_in_event(self, queryset, name, value):
         event = Event.objects.filter(id=value).first()
         number_disciplines = event.disciplines.count()
+
+        # no disciplines just returns the ones still not in that event
         if number_disciplines == 0:
             return queryset.exclude(general_events__id=value)
         
+        # if the event is an encounter all students are retrieved
+        if event.encounter:
+            return queryset.annotate(
+                discipline_count=Count('disciplines_indiv', filter=Q(disciplines_indiv__event=event), distinct=True)
+                ).filter(
+                    discipline_count__lt=1
+                    )
+
+        # if event is not an encounter (competition) only competitior are retrieved
         return queryset.annotate(
             discipline_count=Count('disciplines_indiv', filter=Q(disciplines_indiv__event=event), distinct=True)
             ).filter(
