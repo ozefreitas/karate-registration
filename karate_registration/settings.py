7219feda762739ba8eaa9de528aa1050f9732c20
José Freitas <73902769+ozefreitas@users.noreply.github.com>
2025-10-28 16:23:27 +0000

    [FIX] try first fix API filters not working

--- karate_registration/settings.py
+++ karate_registration/settings.py
@@ -106,7 +106,8 @@ CHANNEL_LAYERS = {
 }
 
 REST_FRAMEWORK = {
-    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    # "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
