f30e90472e285b7bddb9f573532d8e3fd29302d7
ozefreitas <jpsfreitas12@gmail.com>
2025-10-29 00:00:21 +0000

    [GOBACK] with athlete filters

--- registration/filters.py
+++ registration/filters.py
@@ -11,32 +11,28 @@ class AthletesFilters(filters.FilterSet):
     in_gender = filters.CharFilter(method='filter_athletes_in_gender')
 
     def filter_athletes_not_in_event(self, queryset, name, value):
-        # event = Event.objects.filter(id=value).first()
-        # number_disciplines = event.disciplines.count()
+        event = Event.objects.filter(id=value).first()
+        number_disciplines = event.disciplines.count()
 
-        # # no disciplines just returns the ones still not in that event
-        # if number_disciplines == 0:
-        #     return queryset.exclude(general_events__id=value)
-
-        # else: 
-            return queryset
+        # no disciplines just returns the ones still not in that event
+        if number_disciplines == 0:
+            return queryset.exclude(general_events__id=value)
         
-        # # if the event is an encounter all students are retrieved
-        # if event.encounter:
-        #     return queryset.annotate(
-        #         discipline_count=Count('disciplines_indiv', filter=Q(disciplines_indiv__event=event), distinct=True)
-        #         ).filter(
-        #             discipline_count__lt=1
-        #             )
+        # if the event is an encounter all students are retrieved
+        if event.encounter:
+            return queryset.annotate(
+                discipline_count=Count('disciplines_indiv', filter=Q(disciplines_indiv__event=event), distinct=True)
+                ).filter(
+                    discipline_count__lt=1
+                    )
 
-        # # if event is not an encounter (competition) only competitior are retrieved
-        # return queryset.annotate(
-        #     discipline_count=Count('disciplines_indiv', filter=Q(disciplines_indiv__event=event), distinct=True)
-        #     ).filter(
-        #         competitor=True,
-        #         discipline_count__lt=number_disciplines
-        #         )
-
+        # if event is not an encounter (competition) only competitior are retrieved
+        return queryset.annotate(
+            discipline_count=Count('disciplines_indiv', filter=Q(disciplines_indiv__event=event), distinct=True)
+            ).filter(
+                competitor=True,
+                discipline_count__lt=number_disciplines
+                )
     
     def filter_athletes_in_category(self, queryset, name, value):
         return queryset.filter(category=value)
